87a9ffeca099fac11609d52183b9378ec44c97f1
vipin <vipin555@gmail.com>
2020-08-06 19:38:58 +0530

    added edit update delete

--- routes/author.js
+++ routes/author.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const Book = require("../models/book");
 /* to get all the authors */
 router.get("/", async (req, res) => {
   let searchOptions = {};
@@ -19,21 +20,84 @@ router.get("/", async (req, res) => {
   }
 });
 /* new author */
+
 router.get("/new", (req, res) => {
   res.render("author/new", { author: new Author() });
 });
+
+// handle post request
 router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
   try {
     const newAuthor = await author.save();
-    res.redirect("authors");
+    res.redirect(`/authors/${newAuthor.id}`);
   } catch {
     res.render("author/new", {
       author: author,
       errorMessage: "Error creating Author",
     });
+  }
+});
+
+/* handle view author */
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: req.params.id }).limit(6).exec();
+    res.render("author/authorView", {
+      author: author,
+      booksByAuthor: books,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+/* handle edit author */
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("author/edit", { author: author });
+  } catch {
+    res.redirect("/authors");
+  }
+});
+
+/* handle update author */
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${req.params.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("author/edit", {
+        author: author,
+        errorMessage: "Error updating Author",
+      });
+    }
+  }
+});
+
+/* handle delete author */
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect(`/authors`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/authors`);
+    }
   }
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -4,17 +4,22 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express(); //creating an instance of express server
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
 const indexRouter = require("./routes/index"); // index router
 const authorRouter = require("./routes/author"); // author router
 const bookRouter = require("./routes/books"); // book router
+
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout"); //layouts
 app.use(expressLayouts);
+app.use(methodOverride("_method")); // override the method through req.params
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false })); // body-parser for parsing the request body
 app.use(express.static("public"));
+
 app.use("/", indexRouter); // index router
 app.use("/authors", authorRouter); // author router
 app.use("/books", bookRouter); // book router
